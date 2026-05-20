d5258bc89c70a44d2506b977173d20316b7965f4
Simonas Kazlauskas <github@kazlauskas.me>
2026-05-20 18:45:51 +0300

    Create ABIv2 experimental feature (#5)
    Co-authored-by: Lucas <lucas.tnagel@gmail.com>

--- feature-set/src/lib.rs
+++ feature-set/src/lib.rs
@@ -89,6 +89,7 @@ pub struct FeatureSnapshot {
     pub define_ltds_fee_only_semantics: bool,
     pub validate_chained_block_id_2: bool,
     pub upgrade_bpf_stake_program_to_v5_1: bool,
+    pub program_runtime_abiv2: bool,
 }
 
 impl From<&AHashMap<Pubkey, u64>> for FeatureSnapshot {
@@ -204,6 +205,7 @@ impl From<&AHashMap<Pubkey, u64>> for FeatureSnapshot {
             define_ltds_fee_only_semantics: is_active(&define_ltds_fee_only_semantics::ID),
             validate_chained_block_id_2: is_active(&validate_chained_block_id_2::ID),
             upgrade_bpf_stake_program_to_v5_1: is_active(&upgrade_bpf_stake_program_to_v5_1::ID),
+            program_runtime_abiv2: is_active(&program_runtime_abiv2::ID),
         }
     }
 }
@@ -370,6 +372,7 @@ impl FeatureSet {
             enable_sha512_syscall: snapshot.enable_sha512_syscall,
             relax_post_exec_min_balance_check: snapshot.relax_post_exec_min_balance_check,
             define_ltds_fee_only_semantics: snapshot.define_ltds_fee_only_semantics,
+            program_runtime_abiv2: snapshot.program_runtime_abiv2,
         }
     }
 }
@@ -1524,6 +1527,9 @@ pub mod set_lamports_per_byte_to_6960 {
 
     pub const LAMPORTS_PER_BYTE: u64 = 6960;
 }
+pub mod program_runtime_abiv2 {
+    solana_pubkey::declare_id!("AB1v2ExperimentaL111111111111111111111111111");
+}
 
 pub mod reduce_slot_time_to_350ms {
     solana_pubkey::declare_id!("iBRL5RuWhw4yqaAZu96RUULHckHTZAoe2b77qaV38JZ");
@@ -2630,6 +2636,10 @@ pub static FEATURE_NAMES: LazyLock<AHashMap<Pubkey, &'static str>> = LazyLock::n
         (
             alpenglow_fast_leader_handover::id(),
             "SIMD-0326: Alpenglow fast leader handover",
+        ),
+        (
+            program_runtime_abiv2::id(),
+            "SIMD-0177: Program runtime ABIv2",
         ),
         /*************** ADD NEW FEATURES HERE ***************/
         /***** ADD NEW FEATURE BOOL TO `FeatureSnapshot` *****/
