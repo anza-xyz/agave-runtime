2ca57696ee752631b4da53a92bdcc662c30d7ab0
Simonas Kazlauskas <github@kazlauskas.me>
2026-05-22 17:33:06 +0300

    Implement the SetBufferLength syscall (#1)

--- transaction-context/src/vm_addresses.rs
+++ transaction-context/src/vm_addresses.rs
@@ -1,14 +1,34 @@
+use crate::{MAX_ACCOUNTS_PER_TRANSACTION, MAX_INSTRUCTION_TRACE_LENGTH};
+
 pub const SHIFT: u32 = 32;
 pub const GUEST_REGION_SIZE: u64 = 1 << SHIFT;
-pub const HEAP_ADDRESS: u64 = 2 * GUEST_REGION_SIZE;
-pub const STACK_ADDRESS: u64 = 3 * GUEST_REGION_SIZE;
-pub const TRANSACTION_FRAME_ADDRESS: u64 = 4 * GUEST_REGION_SIZE;
-pub const ACCOUNT_METADATA_AREA: u64 = 5 * GUEST_REGION_SIZE;
-pub const INSTRUCTION_TRACE_AREA: u64 = 6 * GUEST_REGION_SIZE;
-pub const RETURN_DATA_SCRATCHPAD: u64 = 7 * GUEST_REGION_SIZE;
-pub const GUEST_ACCOUNT_PAYLOAD_BASE_ADDRESS: u64 = 8 * GUEST_REGION_SIZE;
-pub const GUEST_INSTRUCTION_DATA_BASE_ADDRESS: u64 = 264 * GUEST_REGION_SIZE;
-pub const GUEST_INSTRUCTION_ACCOUNT_BASE_ADDRESS: u64 = 328 * GUEST_REGION_SIZE;
+pub const TRANSACTION_FRAME_ADDRESS: u64 = from_index(4);
+pub const ACCOUNT_METADATA_AREA: u64 = from_index(5);
+pub const INSTRUCTION_TRACE_AREA: u64 = from_index(6);
+pub const RETURN_DATA_SCRATCHPAD: u64 = from_index(7);
+
+pub const GUEST_ACCOUNT_PAYLOAD_BASE_ADDRESS: u64 = from_index(8);
+pub const GUEST_ACCOUNT_PAYLOAD_END_ADDRESS: u64 =
+    GUEST_ACCOUNT_PAYLOAD_BASE_ADDRESS + from_index(MAX_ACCOUNTS_PER_TRANSACTION as u64);
+
+pub const GUEST_INSTRUCTION_DATA_BASE_ADDRESS: u64 =
+    nonoverlapping_base_address(264, GUEST_ACCOUNT_PAYLOAD_END_ADDRESS);
+pub const GUEST_INSTRUCTION_DATA_END_ADDRESS: u64 =
+    GUEST_INSTRUCTION_DATA_BASE_ADDRESS + from_index(MAX_INSTRUCTION_TRACE_LENGTH as u64);
+
+pub const GUEST_INSTRUCTION_ACCOUNT_BASE_ADDRESS: u64 =
+    nonoverlapping_base_address(328, GUEST_INSTRUCTION_DATA_END_ADDRESS);
+pub const GUEST_INSTRUCTION_ACCOUNT_END_ADDRESS: u64 =
+    GUEST_INSTRUCTION_ACCOUNT_BASE_ADDRESS + from_index(MAX_INSTRUCTION_TRACE_LENGTH as u64);
+
+pub const fn from_index(index: u64) -> u64 {
+    index.checked_mul(GUEST_REGION_SIZE).unwrap()
+}
+
+pub const fn nonoverlapping_base_address(base_index: u64, previous_address: u64) -> u64 {
+    assert!(from_index(base_index) >= previous_address);
+    from_index(base_index)
+}
 
 pub const fn abiv2_region_index_from_vm_address(vm_base_address: u64) -> usize {
     (vm_base_address >> SHIFT) as usize
